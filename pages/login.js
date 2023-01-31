99a4451298ffe73496b2667eb39694725f7ba26e
AJ Espinosa <espinosa.aaron45@gmail.com>
2023-01-31 22:52:54 +0800

    Added pages

---
+++ pages/login.js
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import { useState } from "react";
+
+const Login = () => {
+    const [username, setUsername] = useState("")
+    const handleFetch = () => {
+        console.log(username)
+    }
+
+    const handleChange = (e) => {
+        setValue(e.target.value)
+    }
+    return (
+        <>
+            <div className="w-full h-screen flex flex-col justify-center items-center gap-y-10 text-black">
+                <div className="flex flex-col items-center">
+                    <p className="font-bold text-4xl">//try login</p>
+                    <p className="text-lg">login using your credentials</p>
+                </div>
+                <form className="flex flex-col p-[12px] border-black border-[2px] rounded-[5px]">
+                    <label for="username">username</label>
+                    <input name="username" id="username" type="text" className="border-black border-[2px] rounded-[5px] p-[4px]" />
+                    <label for="password">password</label>
+                    <input name="password" id="password" type="password" className="border-black border-[2px] rounded-[5px] p-[4px]" />
+                    <button className="font-bold py-[12px] px-5 border-black border-[2px] rounded-[5px] uppercase items-center justify-center flex mt-[12px]" onClick={handleFetch}>Login</button>
+                </form>
+                <Link href="/register"><p>i want to join</p></Link>
+            </div>
+        </>
+    );
+}
+
+export default Login;
